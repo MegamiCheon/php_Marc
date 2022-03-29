594ad00d4bd36a7449d06cd174de512fa379c056
Megami.ay <megami.ay@gmail.com>
2022-03-29 14:41:05 -0300

    002++

--- 002/index.php
+++ 002/index.php
@@ -1,10 +1,16 @@
 <?php 
 
+define("age", 16);
+
 $msg = "mundo!";
+
+// a var não pode mudar, nem de minusculo para maiusculo
 
 echo "Olá, $msg <br>";
 echo "Olá, $msg";
 echo "<br>";
-echo "Olá " .$msg. "agora esta chovendo.";
+echo "Eu tenho " .age. " anos<br>"
+
+echo "Olá " .$msg. "<br> Meu nome é Yoshikage Kira. Tenho 33 anos. Minha casa fica na parte nordeste de Morioh, onde todas as casas estão, e eu não sou casado. Eu trabalho como funcionário das lojas de departamentos Kame Yu e chego em casa todos os dias às oito da noite, no máximo. Eu não fumo, mas ocasionalmente bebo. Estou na cama às 23 horas e me certifico de ter oito horas de sono, não importa o que aconteça. Depois de tomar um copo de leite morno e fazer cerca de vinte minutos de alongamentos antes de ir para a cama, geralmente não tenho problemas para dormir até de manhã. Assim como um bebê, eu acordo sem nenhum cansaço ou estresse pela manhã. Foi-me dito que não houve problemas no meu último check-up. Estou tentando explicar que sou uma pessoa que deseja viver uma vida muito tranquila. Eu cuido para não me incomodar com inimigos, como ganhar e perder, isso me faria perder o sono à noite. É assim que eu lido com a sociedade e sei que é isso que me traz felicidade. Embora, se eu fosse lutar, não perderia para ninguém.<br>";
 
 ?>
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php 
+
+// ++ incrementa 1 no valor da variavel
+
+
+define("PI", 3.14, false);
+
+echo "O valor de PI é: " .PI. 
+
+?>
