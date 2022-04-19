bb49a03cf2a2ff661c1b38bcd03a48896050df6c
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-04-19 16:23:07 -0300

    006 desafio

--- 006/Formularios/index.php
+++ 006/Formularios/index.php
@@ -1,17 +1,26 @@
 <?php 
     require('header.php')
 ?>
-    <form action="receber.php" method="get">
+
+<form action="receber.php" method="get">
+    <h1>O que você achou do site?</h1>
+    <input type="checkbox" name="apro" value="MB"> Muito bom 
+    <input type="checkbox" name="apro" value="B"> Bom 
+    <input type="checkbox" name="apro" value="R"> Regular 
+    <input type="checkbox" name="apro" value="UL"> Um Lixo <br>
+
+    <h1>Qual a seção que você mais gostou?</h1>
+--------------------------------------------------------------------------
     <label for="nome">Nome: </label>
     <input type="text" name="nome"><br>
 
     <label for="senha">Senha: </label>
     <input type="text" name="senha"><br>
 
-    <input type="checkbox" name="est" value="Estudante"> Estudante <br>
+    
 
     <input type="submit" value="Enviar">
-    </form>
+</form>
 
 <?php 
     require('footer.php')
--- 006/Formularios/receber.php
+++ 006/Formularios/receber.php
@@ -1,21 +1,23 @@
 <?php 
 
-   $nome = filter_input(INPUT_GET, 'nome');
-   $senha = filter_input(INPUT_GET, 'senha');
-   $check = filter_input(INPUT_GET, 'est');
 
-    if ($nome && $senha){
-        echo "Nome: ".$nome."<br>";
-        echo "Senha: ".$senha."<br>";
-    } else {
-        echo "Valores Inválidos";
-    } echo "<br>";
 
-    if ($check){
-        echo "Cargo: ".$check;
-    } else {
-        echo "Ele não é estudante";
-    }
+//    $nome = filter_input(INPUT_GET, 'nome');
+//    $senha = filter_input(INPUT_GET, 'senha');
+//    $check = filter_input(INPUT_GET, 'est');
+
+//     if ($nome && $senha){
+//         echo "Nome: ".$nome."<br>";
+//         echo "Senha: ".$senha."<br>";
+//     } else {
+//         echo "Valores Inválidos";
+//     } echo "<br>";
+
+//     if ($check){
+//         echo "Cargo: ".$check;
+//     } else {
+//         echo "Ele não é estudante";
+//     }
     
 //    echo "Nome: ".$nome."<br>";
 //    echo "Senha: ".$senha
