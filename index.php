6f3697c4aaca4a7d0f5c1fc5c115d1dd3f16ec11
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-04-26 16:37:43 -0300

    index redi

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-    require("./Site/template/header.php");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Site/css/style.css">
-    <title>Site</title>
-</head>
-<body>
-    <h1>Index</h1>
-</body>
-</html>
-
-<?php
-    require("./Site/template/footer.php");
+    header("Location: "."./Site/pag/caduser.php");
+    exit;
 ?>
