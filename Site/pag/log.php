53f74be473104ce5eada4aaef2a8dc120da56696
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-05-17 08:40:54 -0300

    log.php... php feito

--- Site/pag/log.php
+++ Site/pag/log.php
@@ -1,25 +1,38 @@
 <?php
     require("../template/header.php");
 
-    //Isset verifica se o arquivo ta vazio
-    if(isset($_COOKIE['nome'])){
-        echo "<h2>Olá</h2>".$_COOKIE['nome']."</h2>";
-    } else {
-        echo "<h2>Não tem informação no cookie</h2>";
+    $email = $senha = "";
+    $emailerr = $senhaerr = "";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if (empty($_POST['email'])){
+            $emailerr = "Email é obrigatório";
+        } else {
+            $email = $_POST["email"];
+        }
+        if (empty($_POST['senha'])){
+            $senhaerr = "Senha é obrigatória";
+        } else {
+            $senha = $_POST['senha'];
+        }
     }
 
 ?>
 
-<form action="" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <fieldset>
         <legend>Login</legend>
         <label for="email">E-mail:</label>
-        <input type="mail" name="email"><br>
+        <input type="mail" name="email">
+        <span>*<?php echo $emailerr?></span><br>
+        
 
         <label for="senha">Senha:</label>
-        <input type="password" name="senha"><br>
+        <input type="password" name="senha">
+        <span>*<?php echo $senhaerr?></span><br>
 
-        <input type="submit" value="Entrar">
+        <input type="submit" value="Entrar"><br>
+        <a href="caduser.php" target="_blank">Cadastrar</a>
 
     </fieldset>
 </form>
