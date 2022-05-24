1bb1bc3db043e5de99e55f11e984bbcc9f49efa1
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-05-24 09:26:58 -0300

    mysql

--- include/MySQL.php
+++ include/MySQL.php
@@ -11,10 +11,9 @@
                         USER,
                         PASS,
                         array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'))
-        $pdo -> setAttibute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e){
-
+        echo 'Erro';
     }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-    header("Location: "."./Site/pag/caduser.php");
+    header("Location: "."./Site/pag/log.php");
     exit;
 ?>
