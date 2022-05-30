01ff91f2c99b29221d079253d86287e008b9a91b
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-05-30 08:51:44 -0300

    a

--- include/MySQL.php
+++ include/MySQL.php
@@ -4,17 +4,18 @@
     define('DB', 'EMI');
     define('USER', 'root');
     define('PASS', '');
+    define('PORT', '3306');
 
     try{
         //conexao
-        $pdo = new PDO('mysql:host='.HOST.';DBNAME='.DB,
+        $pdo = new PDO('mysql:host='.HOST.';port'.PORT.';DBNAME='.DB,
                         USER,
                         PASS,
-                        array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'))
-        $pdo -> setAttibute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        array(PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'));
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch (Exception $e){
-
+        echo 'Erro';
     }
 
 ?>
