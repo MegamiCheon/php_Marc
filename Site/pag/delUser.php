d5fa4b4f5d49c0673f6ff8b78458827bf81ef7ff
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-06-13 11:11:12 -0300

    a

--- Site/pag/delUser.php
+++ Site/pag/delUser.php
@@ -6,7 +6,13 @@
         $code = $_GET['code'];
         echo "Código: $code";
 
-
+        $sql = $pdo -> prepare("DELETE FROM user WHERE code=?");
+        if ($sql->execute(array($code))){
+            echo "Usuário excluído com sucesso";
+            header('location:listuser.php')
+        } else {
+            echo "Erro: Dados não foram excluídos <br>";
+            echo "Comando: $sql. <br>";
+        }
     }
-
 ?>
