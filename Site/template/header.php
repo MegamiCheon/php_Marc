6057c842f254ce7052626617e79865d3ba61c465
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-06-28 14:29:16 -0300

    aa

--- Site/template/header.php
+++ Site/template/header.php
@@ -7,15 +7,18 @@
 <link rel="stylesheet" href="../css1/style.css">
 <body>
     <header>
-        <h1 class="Logo">Menu</h1>
-        <div class="header">
-        <ul class="menuzin">
-            <li class="menu-item"><a href="#">Produto</a></li>
-            <li class="menu-item"><a href='../pag/main.php'>Usuário</a></li>
-            <li class="menu-item"><a href='../pag/logout.php'>Logout</a></li>
-        </ul>
+        <div class="GAY">
+            <h1 class="Logo">Menu</h1>
+            <div class="header">
+            <ul class="menuzin">
+                <li class="menu-item"><a href="#">Produto</a></li>
+                <li class="menu-item"><a href='../pag/main.php'>Usuário</a></li>
+                <li class="menu-item"><a href='../pag/logout.php'>Logout</a></li>
+            </ul>
+            </div>
         </div>
         <div class="clear"></div>
         <hr>
+        <p>gglj</p>
     </header>
 </body>
